51c9fd420f4dab0712cc1718cd7b5143d45ccab3
jgibb062 <jgibb062@gmail.com>
2023-07-04 21:56:15 -0700

    knex greatness

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 // Update with your config settings.
+// require('dotenv').config()
 
+// const connectionString = process.env.DB_CONNECTION_STRING;
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
