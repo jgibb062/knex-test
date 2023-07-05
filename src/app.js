736cb0d1de6263c6ff8a15dea29789bbac434f36
jgibb062 <jgibb062@gmail.com>
2023-07-04 21:39:14 -0700

    express

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8081;
+const knex = require('knex')(require('../knexfile.js')['development'])
 
+
+app.get('/', (req, res) => {
+    res.send('App up and running')
+})
+
+app.listen(port, () => {
+    console.log('Your Knex and Express app are running successfully.')
+})
+
+app.get('/pets', (req, res) => {
+    knex('pet')
+        .select('*')
+        .then(pets => {
+            var petNames = pets.map(pet => pet.name)
+            res.json(petNames);
+        })
+
+})
